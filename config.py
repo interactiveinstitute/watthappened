8366ebef23c3a705ae9eaac177877017fc692b03
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2013-05-23 09:33:02 +0200

    Remove unused configuration

--- config.py
+++ config.py
@@ -5,17 +5,5 @@ COUCHDB_USER = '' # FIXME
 COUCHDB_PASSWORD = '' # FIXME
 COUCHDB_DATABASE = 'sp'
 
-NON_DATASTREAM_FIELDS = ('timestamp', 'field', '_id', '_rev', '_deleted')
-
-FIELDS = (
-        'at',
-        'ElectricPower',
-        'OfficeOccupied',
-        'OfficeTemperature',
-        'ElectricEnergy',
-        'ElectricEnergyOccupied',
-        'ElectricEnergyUnoccupied'
-      )
-
 # Set this to n to also provide the last n changes to your change handler.
 DEBUG_USE_LAST_CHANGES = 500
