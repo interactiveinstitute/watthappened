c95eb33105c96ab3740a64e25af80dfce0f9e142
Sander Dijkhuis <mail@sanderdijkhuis.nl>
2013-06-10 15:27:39 +0200

    Add some Sphinx docs

--- energykit/datasource.py
+++ energykit/datasource.py
@@ -6,9 +6,20 @@ class DataSource(object):
     self._instances = {}
 
   def get_stream_keys(self, allow_cached=True):
+    '''
+    :param bool allow_cached: Set to False if a fresh index is required.
+
+    :returns: Keys that can be used with :meth:`get_stream_by_key`.
+    :rtype: list or generator
+    '''
     raise NotImplementedError
 
   def get_stream_by_key(self, key):
+    '''
+    :param key: A key as specified by the implementation.
+    :returns: The only instance with key :attr:`key`, possibly just generated.
+    :rtype: an instance of a :class:`energykit.datastream.DataStream` subclass
+    '''
     if not key in self._instances:
       self._instances[key] = self._create_stream_instance_by_key(key)
     return self._instances[key]
